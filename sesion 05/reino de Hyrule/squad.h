9e4245bdd5601bfd667d4a77b5ce5065b49dca6a
JOSE VILLALOBOS MONTERO <jose.villalobos.montero@est.una.ac.cr>
2026-03-25 15:52:11 -0600

    c

--- sesion 05/reino de Hyrule/squad.h
+++ sesion 05/reino de Hyrule/squad.h
@@ -1,16 +1,30 @@
 #ifndef SQUAD_H
 #define SQUAD-H
 
+#include "warrior.h"
+#include<string>
+
 namespace EIF201 {
-	class squad
+	class Squad
 	{
 	private:
 
-
-
-
+		std::string name;
+		Warrior** wariors;
+		int capacity;
+		int count;
+		void expandCapacity();
 	public:
-
+		Squad(std::string name, int initialCapacity = 3);
+		~Squad();
+		Warrior* recruit(std::string name, std::string cls, int atk, int def, int hp, int maxSkills);
+		bool totalPower()const;
+		void showSquad()const;
+		int getCont() const;
+		std::string getName()const;
+		Warrior** getByClass(std::string cls, int& resultCount)const;
+		void sortByPower();
+		std::string simulateBattle(const Squad& enemy)const;
 
 	};
 }
