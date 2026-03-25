42d658905940d5477cbc811192935cac2d286767
JOSE VILLALOBOS MONTERO <jose.villalobos.montero@est.una.ac.cr>
2026-03-25 15:58:27 -0600

    x

--- sesion 05/reino de Hyrule/squad.cpp
+++ sesion 05/reino de Hyrule/squad.cpp
@@ -1,3 +1,49 @@
-#include "squad.h"
+#include "Squad.h"
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+using namespace EIF201;
+void Squad::expandCapacity()
+{
+}
 
+Squad::Squad(std::string name, int initialCapacity)
+{
+}
 
+Squad::~Squad()
+{
+}
+
+Warrior* Squad::recruit(std::string name, std::string cls, int atk, int def, int hp, int maxSkills)
+{
+	return nullptr;
+}
+
+bool Squad::totalPower() const
+{
+	return false;
+}
+
+void Squad::showSquad() const
+{
+}
+
+int Squad::getCont() const
+{
+	return 0;
+}
+
+Warrior** Squad::getByClass(std::string cls, int& resultCount) const
+{
+	return nullptr;
+}
+
+void Squad::sortByPower()
+{
+}
+
+std::string Squad::simulateBattle(const Squad& enemy) const
+{
+	return std::string();
+}
